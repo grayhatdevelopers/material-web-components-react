2fd4ddcfd211f46761336fafabbe9bf3b13529cf
Saad A. Bazaz <saadbazaz@hotmail.com>
2024-08-02 12:41:30 +0500

    chore: prettify layout

--- apps/demo/src/app/layout.tsx
+++ apps/demo/src/app/layout.tsx
@@ -1,24 +1,24 @@
-import 'material-symbols';
-import '@lit-labs/ssr-react/enable-lit-ssr.js';
-import GitHubButton from '../components/GitHubButton';
-// import NavigationDrawer from "@packages/ui/src/navigation-drawer"
+import "material-symbols";
+import "@lit-labs/ssr-react/enable-lit-ssr.js";
+import GitHubButton from "../components/GitHubButton";
 
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 
 // @TODO: Get static fonts to work somehow, to prevent FOUC
-const roboto = Roboto({ 
+const roboto = Roboto({
   weight: ["400", "500", "700"],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-roboto',
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
 });
 
 export const metadata: Metadata = {
   title: "Demo - Material Web Components for React",
-  description: "Fast, performant, Material 3 compliant. The lightest Material library out there for ReactJS.",
+  description:
+    "Fast, performant, Material 3 compliant. The lightest Material library out there for ReactJS.",
 };
 
 export default function RootLayout({
@@ -37,19 +37,21 @@ export default function RootLayout({
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </head>
       <body>
-      <main className="bg-[#FDF7FF] max-h-screen w-full">
-      <div
-        id="titlebar"
-        className="backdrop-blur-md z-10 w-full h-16 flex flex-row gap-3 items-center justify-center"
-      >
-        <h1 className="font-bold text-xl">Material Web Components for React</h1>
-        <GitHubButton href="https://github.com/grayhatdevelopers/material-web-components-react">
-          Star us on GitHub
-        </GitHubButton>
-      </div>
-      <div className="flex flex-col justify-center items-center md:grid md:grid-cols-[1fr_1fr] lg:grid-cols-[22.5rem_1fr_1fr] gap-4 md:h-screen">
-        {children}
-        </div>
+        <main className="bg-[#FDF7FF] max-h-screen w-full">
+          <div
+            id="titlebar"
+            className="backdrop-blur-md z-10 w-full h-16 flex flex-row gap-3 items-center justify-center"
+          >
+            <h1 className="font-bold text-xl">
+              Material Web Components for React
+            </h1>
+            <GitHubButton href="https://github.com/grayhatdevelopers/material-web-components-react">
+              Star us on GitHub
+            </GitHubButton>
+          </div>
+          <div className="flex flex-col justify-center items-center md:grid md:grid-cols-[1fr_1fr] lg:grid-cols-[22.5rem_1fr_1fr] gap-4 md:h-screen">
+            {children}
+          </div>
         </main>
       </body>
     </html>
