cb052adf7ed3222d5e9d3f0843a3e1f8e8e8b444
Saad A. Bazaz <saadbazaz@hotmail.com>
2024-12-17 19:27:26 +0500

    fix(test): change tiny glob to fast glob

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,16 +1,14 @@
-import glob from "tiny-glob";
+import fg from "fast-glob";
 import { defineConfig } from "tsup";
 
 export default defineConfig(async (options) => ({
   clean: true,
-  entry: await glob("./src/**/!(*.d|*.spec).tsx"),
+  entry: await fg("./src/**/!(*.d|*.spec).tsx"),
   splitting: true,
   target: "es5",
   format: "esm",
-  // dts: true,
   treeshake: true,
   bundle: true,
-  // sourcemap: true,
   outDir: "./dist",
   minify: !options.watch,
 }));
